58bdf6dc7bd0cee71072fb192d848272bcd2cdf3
mustafa3264 <fanghailiang2016@gmail.com>
2025-07-20 20:30:01 +0800

    test

--- src/renderer/components/nav.tsx
+++ src/renderer/components/nav.tsx
@@ -1,12 +1,200 @@
 import { Component, ReactNode } from 'react';
+import {
+  SettingOutlined,
+  OneToOneOutlined,
+  LineChartOutlined,
+  FlagOutlined,
+} from '@ant-design/icons';
 import { connect } from 'react-redux';
-import { Layout, Menu, Flex } from "antd";
+import { Button, Layout, Menu, Flex } from "antd";
+import { cloneDeep } from 'lodash';
 
+import { langTrans } from '@lang/i18n';
+import { 
+  TABLE_VERSION_ITERATION_FIELDS,
+  TABLE_MICRO_SERVICE_FIELDS,
+} from '@conf/db';
 import { SET_NAV_COLLAPSED } from '@conf/redux';
+import {
+  NETWORK,
+  SETTINGS,
+  ITERATOR,
+  PROJECT,
+} from '@conf/global_config';
+import {
+  ENV_LIST_ROUTE,
+  PROJECT_LIST_ROUTE,
+  VERSION_ITERATOR_LIST_ROUTE,
+  INTERNET_REQUEST,
+  REQUEST_HISTORY,
+  ENVVAR_GLOBAL_LIST_ROUTE,
+} from '@conf/routers';
+import { getOpenVersionIterators } from "@act/version_iterator";
+import { getPrjs } from "@act/project";
+
+let version_iterator_uuid = TABLE_VERSION_ITERATION_FIELDS.FIELD_UUID;
+let version_iterator_title = TABLE_VERSION_ITERATION_FIELDS.FIELD_NAME;
+
+let prj_label = TABLE_MICRO_SERVICE_FIELDS.FIELD_LABEL;
+let prj_remark = TABLE_MICRO_SERVICE_FIELDS.FIELD_REMARK;
 
 const { Sider } = Layout;
 
 class Nav extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          navs: [
+            {
+              key: NETWORK,
+              icon: <OneToOneOutlined />,
+              label: langTrans("nav request"),
+              children:[
+                {
+                  key: INTERNET_REQUEST,
+                  label: (
+                    <a href={ "#" + INTERNET_REQUEST } rel="noopener noreferrer">
+                      {langTrans("nav request send")}
+                    </a >
+                  )
+                },
+                {
+                  key: REQUEST_HISTORY,
+                  label: (
+                    <a href={ "#" + REQUEST_HISTORY } rel="noopener noreferrer">
+                      {langTrans("nav request log")}
+                    </a >
+                  )
+                }
+              ]
+            },
+            {
+              key: ITERATOR,
+              icon: <LineChartOutlined />,
+              label: <Button style={{padding: 0}} type='text' onClick={() => {
+                if (this.props.iterations.length === 0) {
+                  getOpenVersionIterators(this.props.clientType, this.props.dispatch);
+                }
+              }}>{langTrans("nav iterator")}</Button>,
+              children: [
+              ]
+            },
+            {
+              key: PROJECT,
+              icon: <FlagOutlined />,
+              label: <Button style={{padding: 0}} type='text' onClick={() => {
+                if (this.props.prjs.length === 0) {
+                  getPrjs(this.props.clientType, this.props.dispatch);
+                }
+              }}>{langTrans("nav project")}</Button>,
+              children: [
+              ]
+            },
+            {
+              key: SETTINGS,
+              icon: <SettingOutlined />,
+              label: langTrans("nav setting"),
+              children: [
+                {
+                  key: VERSION_ITERATOR_LIST_ROUTE,
+                  label:(
+                    <a href={ "#" + VERSION_ITERATOR_LIST_ROUTE } rel="noopener noreferrer">
+                      {langTrans("nav setting iterator")}
+                    </a>
+                  )
+                },
+                {
+                  key: ENVVAR_GLOBAL_LIST_ROUTE,
+                  label: (
+                    <a href={ "#" + ENVVAR_GLOBAL_LIST_ROUTE } rel="noopener noreferrer">
+                      {langTrans("nav setting envvar")}
+                    </a >
+                  )
+                },
+                {
+                  key: PROJECT_LIST_ROUTE,
+                  label: (
+                    <a href={ "#" + PROJECT_LIST_ROUTE } rel="noopener noreferrer">
+                      {langTrans("nav setting project")}
+                    </a >
+                  )
+                },
+                {
+                  key: ENV_LIST_ROUTE,
+                  label: (
+                    <a href={ "#" + ENV_LIST_ROUTE } rel="noopener noreferrer">
+                      {langTrans("nav setting env")}
+                    </a >
+                  )
+                },
+              ]
+            },
+          ],
+          initPrjsFlg: false,
+          initIterationFlg: false,
+        };
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {  
+        if (!prevState.initPrjsFlg && nextProps.prjs.length > 0 && prevState.navs[2].children.length === 0 ) {
+          let newSstate = cloneDeep(prevState);
+          newSstate.navs[2].children = nextProps.prjs.map((prj) => {
+            return {
+              key: prj.value,
+              label: prj.label,
+              children: [
+                {
+                  key: prj[prj_label] + "_envvar",
+                  label: <a href={"#/prj_envvars/" + prj.value } rel="noopener noreferrer">{langTrans("nav project envvar")}</a >
+                },
+                {
+                  key: prj[prj_label] + "_doc",
+                  label: <a href={"#/project_requests/" + prj.value } rel="noopener noreferrer">{langTrans("nav project doc")}</a >
+                },
+                {
+                  key: prj[prj_label] + "_params",
+                  label: <a href={"#/project_params/" + prj.value } rel="noopener noreferrer">{langTrans("nav project params")}</a >
+                },
+                {
+                  key: prj[prj_label] + "_unittest",
+                  label: <a href={"#/project_tests/" + prj.value } rel="noopener noreferrer">{langTrans("nav project unittest")}</a >
+                }
+              ]
+            }
+          });
+          return {initPrjsFlg: true, navs: newSstate.navs};
+        } if (!prevState.initIterationFlg && nextProps.iterations.length > 0 && prevState.navs[1].children.length === 0 ) {
+          let newSstate = cloneDeep(prevState);
+          newSstate.navs[1].children = nextProps.iterations.map(iteration => {
+            return {
+              key: ITERATOR + "_" + iteration[version_iterator_uuid],
+              label: iteration[version_iterator_title],
+              children: [
+                {
+                  key: ITERATOR + "_" + iteration[version_iterator_uuid] + "_envvar",
+                  label: <a href={"#/iterator_envvars/" + iteration[version_iterator_uuid] } rel="noopener noreferrer">{langTrans("nav iterator envvar")}</a >
+                },
+                {
+                  key: ITERATOR + "_" + iteration[version_iterator_uuid] + "_doc",
+                  label: <a href={"#/version_iterator_requests/" + iteration[version_iterator_uuid] } rel="noopener noreferrer">{langTrans("nav iterator doc")}</a >
+                },
+                {
+                  key: ITERATOR + "_" + iteration[version_iterator_uuid] + "_unittest",
+                  label: <a href={"#/version_iterator_tests/" + iteration[version_iterator_uuid] } rel="noopener noreferrer">{langTrans("nav iterator unittest")}</a >
+                },
+                {
+                  key: ITERATOR + "_" + iteration[version_iterator_uuid] + "_vip",
+                  label: <a href={"#/version_iterator_vip/" + iteration[version_iterator_uuid] } rel="noopener noreferrer">{langTrans("nav iterator member")}</a >
+                }
+              ],
+            }
+          });
+          return {initIterationFlg: true, navs: newSstate.navs};
+        } 
+        return null;
+    }
 
     setCollapsed = (collapsed) => {
       this.props.dispatch({
@@ -41,7 +229,12 @@ class Nav extends Component {
                 marginRight: 9
               }}>{this.props.appVersion}</p>
               </Flex>
-            <Menu theme="dark" defaultSelectedKeys={this.props.selected} mode="inline" items={this.props.navs} />
+              <Menu 
+                theme="dark" 
+                defaultSelectedKeys={this.props.selected} 
+                mode="inline" 
+                items={this.state.navs} 
+                />
           </Sider>
         );
     }
@@ -51,10 +244,12 @@ function mapStateToProps (state) {
   return {
     uuid: state.device.uuid,
     selected: state.nav.selected,
-    navs: state.nav.navs,
     appName: state.device.appName,
     appVersion: state.device.appVersion,
     collapsed: state.nav.collapsed,
+    prjs: state.prj.list,
+    iterations: state.version_iterator.list,
+    clientType: state.device.clientType,
   }
 }
 
--- src/renderer/reducers/redux/nav.tsx
+++ src/renderer/reducers/redux/nav.tsx
@@ -1,124 +1,16 @@
-import { cloneDeep } from 'lodash';
-
-import {
-  SettingOutlined,
-  OneToOneOutlined,
-  LineChartOutlined,
-  FlagOutlined,
-} from '@ant-design/icons';
-
 import {
   NETWORK,
-  SETTINGS,
-  ITERATOR,
-  PROJECT,
 } from '@conf/global_config';
 
-import { 
-  TABLE_VERSION_ITERATION_FIELDS,
-  TABLE_MICRO_SERVICE_FIELDS,
-} from '@conf/db';
-
 import {
-  ENV_LIST_ROUTE,
-  PROJECT_LIST_ROUTE,
-  VERSION_ITERATOR_LIST_ROUTE,
   INTERNET_REQUEST,
-  REQUEST_HISTORY,
-  ENVVAR_GLOBAL_LIST_ROUTE,
 } from '@conf/routers';
 
 import { 
-  GET_VERSION_ITERATORS,
   SET_NAV_COLLAPSED,
-  GET_PRJS
 } from '@conf/redux';
-import { langTrans } from '@lang/i18n';
-let version_iterator_uuid = TABLE_VERSION_ITERATION_FIELDS.FIELD_UUID;
-let version_iterator_title = TABLE_VERSION_ITERATION_FIELDS.FIELD_NAME;
-
-let prj_label = TABLE_MICRO_SERVICE_FIELDS.FIELD_LABEL;
-let prj_remark = TABLE_MICRO_SERVICE_FIELDS.FIELD_REMARK;
 
 export default function (state = {
-    navs: [
-      {
-        key: NETWORK,
-        icon: <OneToOneOutlined />,
-        label: langTrans("nav request"),
-        children:[
-          {
-            key: INTERNET_REQUEST,
-            label: (
-              <a href={ "#" + INTERNET_REQUEST } rel="noopener noreferrer">
-                {langTrans("nav request send")}
-              </a >
-            )
-          },
-          {
-            key: REQUEST_HISTORY,
-            label: (
-              <a href={ "#" + REQUEST_HISTORY } rel="noopener noreferrer">
-                {langTrans("nav request log")}
-              </a >
-            )
-          }
-        ]
-      },
-      {
-        key: ITERATOR,
-        icon: <LineChartOutlined />,
-        label: langTrans("nav iterator"),
-        children: [
-        ]
-      },
-      {
-        key: PROJECT,
-        icon: <FlagOutlined />,
-        label: langTrans("nav project"),
-        children: [
-        ]
-      },
-      {
-        key: SETTINGS,
-        icon: <SettingOutlined />,
-        label: langTrans("nav setting"),
-        children: [
-          {
-            key: VERSION_ITERATOR_LIST_ROUTE,
-            label:(
-              <a href={ "#" + VERSION_ITERATOR_LIST_ROUTE } rel="noopener noreferrer">
-                {langTrans("nav setting iterator")}
-              </a>
-            )
-          },
-          {
-            key: ENVVAR_GLOBAL_LIST_ROUTE,
-            label: (
-              <a href={ "#" + ENVVAR_GLOBAL_LIST_ROUTE } rel="noopener noreferrer">
-                {langTrans("nav setting envvar")}
-              </a >
-            )
-          },
-          {
-            key: PROJECT_LIST_ROUTE,
-            label: (
-              <a href={ "#" + PROJECT_LIST_ROUTE } rel="noopener noreferrer">
-                {langTrans("nav setting project")}
-              </a >
-            )
-          },
-          {
-            key: ENV_LIST_ROUTE,
-            label: (
-              <a href={ "#" + ENV_LIST_ROUTE } rel="noopener noreferrer">
-                {langTrans("nav setting env")}
-              </a >
-            )
-          },
-        ]
-      },
-    ],
     selected: [ NETWORK, INTERNET_REQUEST ],
     collapsed: false,
 }, action : object) {
@@ -128,87 +20,7 @@ export default function (state = {
       return Object.assign({}, state, {
         collapsed: newCollapsed
       });
-    case GET_VERSION_ITERATORS:
-      let verIteratorNavs = cloneDeep(state.navs);
 
-      let selectedVersionIteratorNav : any;
-      for (let nav of verIteratorNavs) {
-        if(nav.key === ITERATOR) {
-          selectedVersionIteratorNav = nav;
-        }
-      }
-      selectedVersionIteratorNav.children = [];
-      let versionIterators = cloneDeep(action.versionIterators);
-
-      for( let versionIterator of versionIterators) {
-        selectedVersionIteratorNav.children.push({
-          key: ITERATOR + "_" + versionIterator[version_iterator_uuid],
-          label: versionIterator[version_iterator_title],
-          children: [
-            {
-              key: ITERATOR + "_" + versionIterator[version_iterator_uuid] + "_envvar",
-              label: <a href={"#/iterator_envvars/" + versionIterator[version_iterator_uuid] } rel="noopener noreferrer">{langTrans("nav iterator envvar")}</a >
-            },
-            {
-              key: ITERATOR + "_" + versionIterator[version_iterator_uuid] + "_doc",
-              label: <a href={"#/version_iterator_requests/" + versionIterator[version_iterator_uuid] } rel="noopener noreferrer">{langTrans("nav iterator doc")}</a >
-            },
-            {
-              key: ITERATOR + "_" + versionIterator[version_iterator_uuid] + "_unittest",
-              label: <a href={"#/version_iterator_tests/" + versionIterator[version_iterator_uuid] } rel="noopener noreferrer">{langTrans("nav iterator unittest")}</a >
-            },
-            {
-              key: ITERATOR + "_" + versionIterator[version_iterator_uuid] + "_vip",
-              label: <a href={"#/version_iterator_vip/" + versionIterator[version_iterator_uuid] } rel="noopener noreferrer">{langTrans("nav iterator member")}</a >
-            }
-          ],
-        });
-      }
-
-      return Object.assign({}, state, {
-        navs: verIteratorNavs
-      });
-      
-    case GET_PRJS:
-      let projectNavs = cloneDeep(state.navs);
-
-      let selectedProjectNav : any;
-      for (let nav of projectNavs) {
-        if(nav.key === PROJECT) {
-          selectedProjectNav = nav;
-        }
-      }
-      selectedProjectNav.children = [];
-
-      let prjs = cloneDeep(action.prjs);
-
-      for( let prj of prjs) {
-        selectedProjectNav.children.push({
-          key: prj[prj_label],
-          label: prj[prj_remark],
-          children: [
-            {
-              key: prj[prj_label] + "_envvar",
-              label: <a href={"#/prj_envvars/" + prj[prj_label] } rel="noopener noreferrer">{langTrans("nav project envvar")}</a >
-            },
-            {
-              key: prj[prj_label] + "_doc",
-              label: <a href={"#/project_requests/" + prj[prj_label] } rel="noopener noreferrer">{langTrans("nav project doc")}</a >
-            },
-            {
-              key: prj[prj_label] + "_params",
-              label: <a href={"#/project_params/" + prj[prj_label] } rel="noopener noreferrer">{langTrans("nav project params")}</a >
-            },
-            {
-              key: prj[prj_label] + "_unittest",
-              label: <a href={"#/project_tests/" + prj[prj_label] } rel="noopener noreferrer">{langTrans("nav project unittest")}</a >
-            }
-          ]
-        });
-      }
-      return Object.assign({}, state, {
-          navs: projectNavs
-      });
     default:
       return state;
   }
