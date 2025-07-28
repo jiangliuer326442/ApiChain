e2343994a09313cdfc04f2eb68cc181f82d476e4
mustafa3264 <fanghailiang2016@gmail.com>
2025-07-29 00:34:31 +0800

    test

--- src/renderer/components/markdown/edit.tsx
+++ src/renderer/components/markdown/edit.tsx
@@ -1,9 +1,7 @@
 import { Component, ReactNode } from 'react';
-import ReactMde from 'react-mde';
+import ReactMde, { ReactMdeTypes } from 'react-mde';
 import * as Showdown from 'showdown';
 import 'react-mde/lib/styles/css/react-mde-all.css';
-
-import './dark-mde.less';
 
 import { isStringEmpty } from '@rutil/index';
 import { langTrans } from '@lang/i18n';
@@ -16,8 +14,9 @@ export default class extends Component {
         let content = props.content;
 
         this.state = {
-            markdown: isStringEmpty(content) ? langTrans("iterator md content") : content,
-            selectedTab: props.mode === "add" ? "write":"preview",
+            mdeState: {
+                markdown: isStringEmpty(content) ? langTrans("iterator md content") : content,
+            },
         }
 
         this.converter = new Showdown.Converter({
@@ -37,14 +36,12 @@ export default class extends Component {
             <ReactMde
                 className="react_me"
                 layout="horizontal"
-                selectedTab={this.state.selectedTab}
-                onTabChange={selectedTab => this.setState({selectedTab})}
-                value={this.state.markdown}
+                editorState={this.state.mdeState}
                 generateMarkdownPreview={markdown => Promise.resolve(this.converter.makeHtml(markdown))}
-                onChange={(markdown: string) => {
-                    this.setState({ markdown });
-                    this.props.cb(markdown);
-                }} 
+                onChange={(mdeState: ReactMdeTypes.MdeState) => {
+                    this.setState({ mdeState });
+                    this.props.cb(mdeState.markdown);
+                }}
             ></ReactMde>
         )
     }
