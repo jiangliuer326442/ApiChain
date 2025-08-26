65e30c8be5ddab1c0bc71cdf373bf6d7a3f74b5b
mustafa3264 <fanghailiang2016@gmail.com>
2025-08-26 21:29:34 +0800

    test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config"
+import "hardhat-deploy"
 import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
@@ -6,6 +7,11 @@ const config: HardhatUserConfig = {
     root: "./src/solidity",
   },
   solidity: "0.8.28",
+  namedAccounts: {
+    deployer: {
+        default: 0,
+    },
+  }
 }
 
 export default config
