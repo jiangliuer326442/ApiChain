38d58bed450b06fb75cd744bc949a23313810cf1
mustafa <jiangliuer3264@gmail.com>
2026-03-24 08:30:26 +0800

    test

--- src/config/startArgs.ts
+++
@@ -1,7 +0,0 @@
-export const ArgsMemberBuySuccess = 'member_buy_success';
-
-export const ArgsTokenBuySuccess = 'token_buy_success';
-
-export const ArgsCreateTeamSuccess = 'create_team_success';
-
-export const ArgsJoinTeamSuccess = 'join_team_success';
--- src/main/processInit/system/reload.ts
+++
@@ -1,9 +0,0 @@
-import { BrowserWindow, ipcMain } from 'electron';
-
-import { ChannelsLoadAppStr } from '../../../config/channel';
-
-export default function (mainWindow : BrowserWindow){
-    ipcMain.on(ChannelsLoadAppStr, (event) => {
-        mainWindow.webContents.reload();
-    });
-}
